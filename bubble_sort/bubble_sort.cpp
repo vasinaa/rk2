43c75673e772037b43e1d1ef3af8e04837cbc7cd
vasinaa <99878647+vasinaa@users.noreply.github.com>
2022-05-26 08:25:10 +0300

    Add files via upload

---
+++ bubble_sort/bubble_sort.cpp
@@ -0,0 +1,16 @@
+template <class T>
+void bubble_sort(T *array, int size) 
+{
+    for (int i = 0; i < size - 1; ++i) 
+    {
+        for (int j = 0; j < size - i - 1; ++j) 
+        {
+            if (array[j] > array[j + 1]) 
+            {
+                T buffer(array[j]);
+                array[j] = array[j + 1];
+                array[j + 1] = buffer;
+            }
+        }
+    }
+}
